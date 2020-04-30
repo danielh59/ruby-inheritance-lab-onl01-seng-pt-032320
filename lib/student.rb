9d7be3ed6145bd9726e7bba6ebc38b33e409b22c
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-30 21:46:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+
+def initialize
+@knowledge = []
+end
+
+def learn(context)
+  @knowledge << context
+
+end
+
+def knowledge
+  @knowledge
+end
 end
